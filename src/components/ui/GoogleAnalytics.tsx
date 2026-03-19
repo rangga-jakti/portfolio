e0eb829499034a9ff8fadf9e683f038657947fc5
Rangga <ranggarvc@gmail.com>
2026-03-19 12:47:46 +0700

    feat: add Google Analytics

--- src/components/ui/GoogleAnalytics.tsx
+++ src/components/ui/GoogleAnalytics.tsx
@@ -1,14 +1,11 @@
-import Script from "next/script";
-
+﻿import Script from "next/script";
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
-
 export default function GoogleAnalytics() {
   if (!GA_ID) return null;
-
   return (
     <>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=`+GA_ID}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -16,9 +13,8 @@ export default function GoogleAnalytics() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${GA_ID}', {
+          gtag('config', '`+GA_ID+`', {
             page_path: window.location.pathname,
-            send_page_view: true,
           });
         `}
       </Script>
